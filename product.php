502849632ff0163c34e92b4a199a1b99e4b94fd3
nitishahuja <nitishahuja971@gmail.com>
2020-09-03 16:16:39 +0530

    ui changes

--- product.php
+++ product.php
@@ -32,35 +32,99 @@
                 <div class="row">
                     <div class="col-sm-6">
                         <img src="./include/images/EKrVUGwXYAAioxe.jpg" alt="" class="img-fluid">
-                        <div class="form-row pt-4 font-size-16">
-                            <div class="col">
-                                <button type="submit" class="btn btn-danger form-control">Buy Now</button>
-                            </div>
-                            <div class="col">
-                                <button type="submit" class="btn btn-info form-control">Add to Cart</button>
-                            </div>
-                        </div>
                     </div>
                     <div class="col-sm-6">
-                        <h5 class="font-size-20 py-3">Modi-"Criticise me"</h5>
-                        <div class="d-flex">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                            <p class="px2 font-size-14">&nbsp; 5000+ rating</p>   
-                        </div>
-                        <hr class="m-0">
+                        <h1 class="font-size-20 py-3"><center>Modi-"Criticise me"</center></h1>
                            <!--product price  -->
-                                <table class="my-3"></table>
-
+                                <center><span class="money">499 ₹</span></center>
                            <!--!product price  -->
+                           <hr class="m-0">
+
+                            <div class="policy">
+                            <div class="return">
+                                <div class="font-size-20 my-2 color-second">
+                                    <span class="fas fa-retweet border p-3 rounded-pill"></span>
+                                </div>
+                                <a href="" class="font-size-12">7 Days<br>Replacement</a>
+                                
+                            </div>
+                            <div class="return">
+                                <div class="font-size-20 my-2 color-second">
+                                    <span class="fas fa-truck border p-3 rounded-pill"></span>
+                                </div>
+                                <a href="" class="font-size-12">Fast<br>Delivery</a>
+                                
+                            </div>
+                            <div class="return">
+                                <div class="font-size-20 my-2 color-second">
+                                    <span class="fas fa-tshirt border p-3 rounded-pill"></span>
+                                </div>
+                                <a href="" class="font-size-12">Premium<br>Quality</a>
+                            </div>
+                            </div>
+                           <!-- form -->
+                            <form action="index.php"> 
+                                <div class="form-row">
+                                    <div class="col">
+                            <label for=""  class="m-1">Style</label>
+                            <select class="form-control">
+                            <option selected="true">Tee</option>
+                            <option disabled="disabled">Hoodie</option>
+                            <option disabled="disabled">Crewneck Sweater</option>
+                            </select>
+                            </div>
+                            <div class="col">
+                            <label for=""  class="m-1">Size&nbsp;&nbsp;&nbsp;<abbr title="size chart"><a href=""><i class="fas fa-info-circle"></i></a></abbr></label>
+                            <select class="form-control">
+                            <option>S</option>
+                            <option selected="true">M</option>
+                            <option>L</option>
+                            <option>XL</option>
+                            <option>XXL</option>
+                            </select>
+                            </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="col">
+                                <label for="" class="m-1">Color</label>
+                            <select class="form-control">
+                            <option>Black</option>
+                            <option>White</option>
+                            </select>
+                                </div>
+                                <div class="col">
+                                <label for="" class="m-1">Quantity</label>
+                                <div class="input-group">
+                                    <span class="input-group-btn">
+                                        <button type="button" class="quantity-left-minus btn btn-dark btn-number" data-type="minus" data-field="">
+                                        <i class="fas fa-minus"></i>
+                                        </button>
+                                    </span>
+                                    <input type="text" id="quantity" name="quantity" class="form-control input-number" value="1" min="1" max="10">
+                                    <span class="input-group-btn">
+                                        <button type="button" class="quantity-right-plus btn btn-dark btn-number" data-type="plus" data-field="">
+                                        <i class="fas fa-plus"></i>
+                                        </button>
+                                    </span>
+                                </div>
+                         </div>
+                            </div>
+                        <div class="form-row pt-4 font-size-16">
+                            <div class="col">
+                                <button type="submit" class="btn btn-primary form-control">Buy Now</button>
+                            </div>
+                            <div class="col">
+                                <button type="submit" class="btn btn-primary form-control">Add to Cart</button>
+                            </div>
+                        </div>
+                        </form>
+                            <!-- !form -->
                     </div>
                   </div>
               </div>
           </div>
         </section>
+        
   <div class="bestseller">
       <div class="container1">
           <h4>
@@ -164,6 +228,42 @@
 <div class="footer">
           <?php include "footer.php"; ?>
       </div>
+      <script>
+          $(document).ready(function(){
+
+var quantitiy=0;
+   $('.quantity-right-plus').click(function(e){
+        
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+        
+        // If is not undefined
+            
+            $('#quantity').val(quantity + 1);
+
+          
+            // Increment
+        
+    });
+
+     $('.quantity-left-minus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+        
+        // If is not undefined
+      
+            // Increment
+            if(quantity>0){
+            $('#quantity').val(quantity - 1);
+            }
+    });
+    
+});
+      </script>
       <script src="include/index.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" inte-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous"></script>
 </body>
